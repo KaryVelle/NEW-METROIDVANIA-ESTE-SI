fbd4fa684f324b438f78ca5f0e32c76785e508ba
AnaRB29 <100314938+AnaRB29@users.noreply.github.com>
2022-10-17 14:13:58 -0500

    AUDIO! (:

--- Assets/Scripts/Contar_1.cs
+++ Assets/Scripts/Contar_1.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -17,6 +18,7 @@ public class Contar_1 : MonoBehaviour
     {
         if (GameObject.FindGameObjectsWithTag("Enemy").Length == 0) //Si la salud del Ghost = 0..
         {
+          
             SceneManager.LoadScene("Level1_2");                     //Cambia de escena a "Level1_2"
         }
         else if(GameObject.FindGameObjectsWithTag("Player").Length == 0)    ////Si la salud del Héroe = 0..
